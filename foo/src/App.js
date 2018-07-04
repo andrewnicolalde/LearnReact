1c161c2a33a3e7a8f721decf0fada0321bc8da9a
Andrew Nicolalde <andrew.nicolalde@gmail.com>
2018-07-04 13:55:26 +0100

    Begin adding states to components

--- foo/src/App.js
+++ foo/src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <Comment>Rampart was such a failure.</Comment>
+        <Comment>Star Wars was once good</Comment>
         <Comment>Mimi is the best!</Comment>
         <Comment>I'm going to Portugal!</Comment>
       </div>
@@ -22,20 +22,62 @@ class App extends Component {
 }
 
 class Comment extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      editing: false
+    }
+    this.edit = this.edit.bind(this);
+    this.save = this.save.bind(this);
+  }
+
   render(){
+    if(this.state.editing){
+      return(this.renderEditing());
+    } else{
+      return(this.renderNormal());
+    }
+  }
+
+  renderNormal(){
+    return(
+    <div className={"commentContainer"}>
+      {this.props.children}
+      <button onClick={this.edit}>Edit</button>
+      <button onClick={this.remove}>Remove</button>
+    </div>
+    );
+  }
+
+  renderEditing(){
     return(
       <div className={"commentContainer"}>
-        {this.props.children}
-        <button onClick={this.edit}>Edit</button>
-        <button onClick={this.remove}>Remove</button>
+        <textarea defaultValue={this.props.children}></textarea>
+        <button onClick={this.save}>Save</button>
       </div>
     );
   }
 
+  /**
+   * This gets called when the user presses the edit button. Turns the text into an editable text field.
+   */
   edit(){
-    alert("You are now editing the comment");
+    this.setState({editing: true});
   }
 
+  /**
+   * This gets called when the user presses the save button. It turns the contents of the text field into a non-changeable
+   * piece of text.
+   */
+  save(){
+    this.setState({editing: false});
+  }
+
+  /**
+   * This gets called when the user presses the remove button. It deletes the entire commentContainer div.
+   * @param e The actual remove button. We remove the entire section by removing its parent, which is the commentContainer.
+   */
   remove(e){
     e.target.parentElement.remove();
   }
